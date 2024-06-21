9dfb2b1e7b153a7509c28448da9f64cc13c0c77e
Andreas-Wimmer <wimmera@fim.uni-passau.de>
2024-06-21 08:41:06 +0200

    Update

--- source/testing_network_loader.py
+++ source/testing_network_loader.py
@@ -2,27 +2,29 @@
 #Piecewise linear and network loader to see how it works before we implement regularized fictitious play 
 #for general networks.
 
-import core.graph as graph
-import core.network as network
-import dynamic_flow as df
-import network_loader as nl
-import utilities.piecewise_linear as pl
-import utilities.right_constant as rc
+from __future__ import annotations
 
-test_graph = graph.DirectedGraph()
-s = graph.Node(1,test_graph)
-v = graph.Node(2, test_graph)
-t = graph.Node(3, test_graph)
+from core.graph import *
+from core.network import Network
+from dynamic_flow import DynamicFlow 
+from network_loader import NetworkLoader
+from utilities.piecewise_linear import PiecewiseLinear
+from utilities.right_constant import RightConstant
+
+test_graph = DirectedGraph()
+s = Node(1,test_graph)
+v = Node(2, test_graph)
+t = Node(3, test_graph)
 
 test_graph.nodes = [s, v, t]
-test_network = network.Network()
+test_network = Network()
 test_network.graph = test_graph
 
 test_network.add_edge(1, 2, 1, 1)
 test_network.add_edge(1, 2, 0, 3)
 test_network.add_edge(2, 3, 0, 2)
 
-net_inflow = rc.RightConstant([0,2],[2])
+net_inflow = RightConstant([0,2],[2])
 test_network.add_commodity((1, net_inflow), 1)
 
 
