52963fc2ea3b4bc733f57ebd4fd11649f92121c6
Andreas-Wimmer <wimmera@fim.uni-passau.de>
2024-07-08 18:35:36 +0200

    Example_16

--- code/testing_monotonicity.py
+++ code/testing_monotonicity.py
@@ -114,10 +114,10 @@ inflow_1 = RightConstant([0,3,5],[1,0.5,0],(0,5))
 inflow_2 = RightConstant([0,3,5],[1,0.5,0],(0,5))
 inflow_3 = RightConstant([0,3,5],[1,0.5,0],(0,5))
 inflow_4 = RightConstant([0,3,5],[0,1.5,0],(0,5))
-inflow_5 = RightConstant([0,5],[0.5, 0],(0,5))
-inflow_6 = RightConstant([0,5],[0.5, 0],(0,5))
-inflow_7 = RightConstant([0,5],[0.5, 0],(0,5))
-inflow_8 = RightConstant([0,5],[1.5, 0],(0,5))
+inflow_5 = RightConstant([0,5],[1, 0],(0,5))
+inflow_6 = RightConstant([0,5],[1, 0],(0,5))
+inflow_7 = RightConstant([0,5],[1, 0],(0,5))
+inflow_8 = RightConstant([0,5],[0, 0],(0,5))
 
 inflow_f = [inflow_1, inflow_2, inflow_3, inflow_4]
 inflow_g = [inflow_5, inflow_6, inflow_7, inflow_8]
