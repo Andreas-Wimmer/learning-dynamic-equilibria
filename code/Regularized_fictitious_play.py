76ddbcdafc0ea54a63cad8f7668fa66367369455
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2024-08-08 16:02:02 +0200

    parallel link networks 4 less steps

--- code/Regularized_fictitious_play.py
+++ code/Regularized_fictitious_play.py
@@ -272,11 +272,11 @@ p_1 = [e_1]
 p_2 = [e_2]
 
 paths = [p_1, p_2]
-net_inflow = RightConstant([0,2],[3,0], (0, 2))
+net_inflow = RightConstant([0,2],[4,0], (0, 2))
 horizon = 2
 delta = 0.25
 epsilon = 0
-numSteps = 500
+numSteps = 50
 lamb = 0.00001
 
 
