89bf7ffcb0c70b4b099e7f34d7b64cf78328f324
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2024-08-08 11:27:39 +0200

    Sign change

--- code/Regularized_fictitious_play.py
+++ code/Regularized_fictitious_play.py
@@ -213,7 +213,7 @@ def reg_fictitious_play(graph: DirectedGraph, cap: List[float], travel: List[flo
                     val_2 = delays_new[i].eval(gap_breaks[j])
                     val_3 = 2*epsilon*(h[(len(gap_breaks) - 1)*i + j] - inflows[i].eval(gap_breaks[j]))
                     val_4 = h[(len(steps) - 1)*i + j] - inflows[i].eval(gap_breaks[j])
-                    sum_1 = sum_1 + (((val_1 + val_2)/2) - val_3)*val_4
+                    sum_1 = sum_1 + (((val_1 + val_2)/2) + val_3)*val_4
             return sum_1
 
         A = []
