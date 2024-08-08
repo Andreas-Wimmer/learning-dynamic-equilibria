98af2a01fbf25e0553ec82d22a8ef843e9c9a17a
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2024-08-08 12:34:01 +0200

    Readd flow_avg

--- code/Regularized_fictitious_play.py
+++ code/Regularized_fictitious_play.py
@@ -176,6 +176,7 @@ def reg_fictitious_play(graph: DirectedGraph, cap: List[float], travel: List[flo
 
         loader_avg = NetworkLoader(network, inflow_dict_avg)
         result_avg = loader_avg.build_flow()
+        flow_avg = next(result_avg)
         delays_avg = loader_avg.path_delay(horizon)
         #5. Calculate the difference of the new flow and the last flow for checking convergence
         diff_inflows = []
