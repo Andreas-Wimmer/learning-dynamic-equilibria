10f55698fcde9a42b5f05640ec7b721bae10d2b5
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2024-08-09 10:46:05 +0200

    Parallel path networks 8 revisited

--- code/Regularized_fictitious_play.py
+++ code/Regularized_fictitious_play.py
@@ -214,7 +214,7 @@ def reg_fictitious_play(graph: DirectedGraph, cap: List[float], travel: List[flo
                     val_2 = delays_avg[i].eval(gap_breaks[j])
                     val_3 = 2*epsilon*(-h[len(gap_breaks)*i + j] + inflow_avg[i].eval(gap_breaks[j]))
                     val_4 = h[len(gap_breaks)*i + j] - inflow_avg[i].eval(gap_breaks[j])
-                    sum_1 = sum_1 + ((val_1 + val_2)/2 + val_3)*val_4
+                    sum_1 = sum_1 + ((val_1 + val_2)/2 - val_3)*val_4
             return sum_1
 
         A = []
