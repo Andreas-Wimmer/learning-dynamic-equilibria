31e12c4ddcccef80cebb6729796d02508d2bb424
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2024-08-09 15:34:41 +0200

    One congested edge per path 4 corrected

--- code/Regularized_fictitious_play.py
+++ code/Regularized_fictitious_play.py
@@ -299,7 +299,7 @@ graph.nodes = {0:s,1:v,2:w,3:x,4:y,5:t}
 graph.edges = [e_1,e_2,e_3,e_4,e_5,e_6,e_7,e_8]
 graph.reversed = False
 
-capacities = [2,1,2,2,1,1,1,3,3]
+capacities = [2,1,2,2,1,1,3,3]
 travel_times = [1,1,1,2,2,1,1,1]
 
 p_1 = [e_1,e_3,e_7]
