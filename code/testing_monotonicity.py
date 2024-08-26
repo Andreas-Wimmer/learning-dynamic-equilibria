f80167ecd4cc0b4c8b3b5fbbccbe3a1d78ce92a9
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2024-08-26 08:54:23 +0200

    Generating non-monotonicity examples 2

--- code/testing_monotonicity.py
+++ code/testing_monotonicity.py
@@ -75,7 +75,7 @@ def monotonicity_check(graph: DirectedGraph, capacities: List[float], travel_tim
             integrals[i] = integrals[i] + value
 
     scalar_product = sum(integrals)
-    term = gap_function.gap_function(inflows_1,delays_1,inflows_2,T)
+    #term = gap_function.gap_function(inflows_1,delays_1,inflows_2,T)
     if abs(scalar_product) < eps:
         scalar_product = 0
     print(str(scalar_product))
@@ -94,16 +94,16 @@ test_graph.edges = [edge_1, edge_2, edge_3]
 
 capacities = [1,3,2]
 travel_times = [1,1,0]
-net_inflow = RightConstant([0,1,1.75,2],[2.5,1,4,0],(0, 2))
+net_inflow = RightConstant([0,1,1.75,2],[3.5,1,4,0],(0, 2))
 
 path_1 = [edge_1, edge_3]
 path_2 = [edge_2, edge_3]
 
 
 inflow_1 = RightConstant([0,0.5,1,2],[1.5,0.5,0,0],(0,2))
-inflow_2 = RightConstant([0,0.5,1,1.75,2],[1,2,1,4,0],(0,2))
+inflow_2 = RightConstant([0,0.5,1,1.75,2],[2,3,1,4,0],(0,2))
 inflow_3 = RightConstant([0,1,1.75,2],[1,0,0,0],(0,2))
-inflow_4 = RightConstant([0,1,1.75,2],[1.5,1,4,0],(0,2))
+inflow_4 = RightConstant([0,1,1.75,2],[2.5,1,4,0],(0,2))
 
 inflow_f = [inflow_1,inflow_2]
 inflow_g = [inflow_3,inflow_4]
