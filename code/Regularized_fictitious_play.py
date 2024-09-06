f0b5786dc3aac27e542debf711043b95595c6bbe
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2024-09-06 11:34:36 +0200

    Checking non monotone games for convergence 2 changing values between 3 and 4 2

--- code/Regularized_fictitious_play.py
+++ code/Regularized_fictitious_play.py
@@ -275,7 +275,7 @@ test_graph.reversed = False
 
 capacities = [1,3,2]
 travel_times = [1,1,0]
-net_inflow = RightConstant([0,1,1.75,2],[3,3,4,0],(0, 2))
+net_inflow = RightConstant([0,1,1.75,2],[3,3.5,4,0],(0, 2))
 
 path_1 = [edge_1, edge_3]
 path_2 = [edge_2, edge_3]
