5b8fc14a8722cb1db82a88628e476ba927d4d528
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2024-09-09 09:48:18 +0200

    Checking non monotone games for convergence 15 1

--- code/Regularized_fictitious_play.py
+++ code/Regularized_fictitious_play.py
@@ -266,32 +266,35 @@ s = Node(0,graph)
 v = Node(1,graph)
 x = Node(2,graph)
 y = Node(3,graph)
-t = Node(4,graph)
+z = Node(4,graph)
+t = Node(5,graph)
 e1 = Edge(s,v,0,graph)
 e2 = Edge(s,x,1,graph)
-e3 = Edge(v,y,2,graph)
-e4 = Edge(x,y,3,graph)
-e5 = Edge(v,t,4,graph)
-e6 = Edge(y,t,5,graph)
-e7 = Edge(x,t,6,graph)
+e3 = Edge(s,y,2,graph)
+e4 = Edge(v,x,3,graph)
+e5 = Edge(x,y,4,graph)
+e6 = Edge(v,z,5,graph)
+e7 = Edge(x,z,6,graph)
+e8 = Edge(y,z,7,graph)
+e9 = Edge(z,t,8,graph)
 
+graph.nodes = {0:s,1:v,2:x,3:y,4:z,5:t}
+graph.edges = [e1,e2,e3,e4,e5,e6,e7,e8,e9]
 
-graph.nodes = {0:s,1:v,2:x,3:y,4:t}
-graph.edges = [e1,e2,e3,e4,e5,e6,e7]
-graph.reversed = False
+capacities = [2,1,3,1,1,2,1,3,2]
+travel_times = [1,1,1,1,1,1,1,1,1]
+net_inflow = RightConstant([0,1,1.75,3],[2.5,1,3,0],(0,3))
 
-capacities = [1,3,1,3,2,2,2]
-travel_times = [1,1,1,1,1,1,1]
-net_inflow = RightConstant([0,1,1.75,2],[4,4,4,0],(0,2))
+p1 = [e1,e6,e9]
+p2 = [e1,e4,e7,e9]
+p3 = [e1,e4,e5,e8,e9]
+p4 = [e2,e7,e9]
+p5 = [e2,e5,e8,e9]
+p6 = [e3,e8,e9]
 
-p1 = [e1,e3,e6]
-p2 = [e2,e4,e6]
-p3 = [e1,e5]
-p4 = [e2,e7]
-
-paths = [p1,p2,p3,p4]
-horizon = 2
-delta = 0.25
+paths = [p1,p2,p3,p4,p5,p6]
+horizon = 3
+delta = 0.5
 epsilon = 0
 numSteps = 1000
 lamb = 0.00000001
