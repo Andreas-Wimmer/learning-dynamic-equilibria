ca7aab238900422942f6bee4a19d60cf986e5410
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2024-10-11 17:36:55 +0200

    Testing the alternative storage function 4

--- code/Regularized_fictitious_play.py
+++ code/Regularized_fictitious_play.py
@@ -345,7 +345,7 @@ graph.edges = [e1,e2,e3]
 
 capacities = [1,2,2]
 travel_times = [1,1,0]
-net_inflow = RightConstant([0,1,1.75,2],[4,4,4,0],(0,2))
+net_inflow = RightConstant([0,1,1.75,2],[6,3,6,0],(0,2))
 
 p1 = [e1,e3]
 p2 = [e2,e3]
