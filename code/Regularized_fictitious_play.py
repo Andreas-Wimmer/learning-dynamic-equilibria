325689343e142c1e2e8b756f59973202c15c4730
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2025-02-20 17:10:28 +0100

    Example 1 non-monotone RFP epsilon 0.1 delta 0.25, 1000 steps, network inflow 3

--- code/Regularized_fictitious_play.py
+++ code/Regularized_fictitious_play.py
@@ -378,8 +378,8 @@ graph.nodes = {0:s,1:v,2:t}
 graph.edges = [e1,e2,e3]
 
 capacities = [1,3,2]
-travel_times = [1,0,0]
-net_inflow = RightConstant([0,2],[6,0],(0,2))
+travel_times = [1,1,0]
+net_inflow = RightConstant([0,2],[3,0],(0,2))
 
 p1 = [e1,e3]
 p2 = [e2,e3]
