53c4dbc83a183496b7a62a4f191d02b8b79957bf
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2025-02-20 17:10:10 +0100

    Non monotonicity check

--- code/testing_monotonicity.py
+++ code/testing_monotonicity.py
@@ -92,8 +92,8 @@ graph.nodes = {0:s,1:v,2:t}
 graph.edges = [e1,e2,e3]
 
 capacities = [1,3,2]
-travel_times = [1,0,0]
-net_inflow = RightConstant([0,2],[5,0],(0,2))
+travel_times = [1,1,0]
+net_inflow = RightConstant([0,2],[3,0],(0,2))
 
 p1 = [e1,e3]
 p2 = [e2,e3]
@@ -102,7 +102,7 @@ paths = [p1,p2]
 horizon = 2
 
 i1 = RightConstant([0,0.5,1,2],[1.5,0.5,0,0],(0,2))
-i2 = RightConstant([0,0.5,1,2],[3.5,4.5,5,0],(0,2))
+i2 = RightConstant([0,0.5,1,2],[1.5,2.5,3,0],(0,2))
 #i3 = RightConstant([0,1,2,5],[1,0,0,0],(0,5))
 #i4 = RightConstant([0,5],[0,0],(0,5))
 #i5 = RightConstant([0,1,2,4,5],[1,2,3,1,0],(0,5))
@@ -120,7 +120,7 @@ i2 = RightConstant([0,0.5,1,2],[3.5,4.5,5,0],(0,2))
 #i17 = RightConstant([0,1,1.75,4],[1,2,1,0],(0,4))
 
 i18 = RightConstant([0,1,2],[1,0,0],(0,2))
-i19 = RightConstant([0,1,2],[4,5,0],(0,2))
+i19 = RightConstant([0,1,2],[2,3,0],(0,2))
 #i20 = RightConstant([0,2,5],[0.5,0,0],(0,5))
 #i21 = RightConstant([0,5],[0,0],(0,5))
 #i22 = RightConstant([0,2,4,5],[1.5,3,1,0],(0,5))
