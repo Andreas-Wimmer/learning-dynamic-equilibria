bc61885213a16644e89e8276ae588b0bed78c2a0
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2025-02-20 17:14:59 +0100

    Non monotonicity check

--- code/Regularized_fictitious_play.py
+++ code/Regularized_fictitious_play.py
@@ -379,7 +379,7 @@ graph.edges = [e1,e2,e3]
 
 capacities = [1,3,2]
 travel_times = [1,1,0]
-net_inflow = RightConstant([0,2],[3,0],(0,2))
+net_inflow = RightConstant([0,],[3,0],(0,2))
 
 p1 = [e1,e3]
 p2 = [e2,e3]
