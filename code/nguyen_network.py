ef91596943d7c78cc8dc37bb999830589905e328
Andreas-Wimmer <163832691+Andreas-Wimmer@users.noreply.github.com>
2025-04-28 09:18:32 +0200

    Fix indexing

--- code/nguyen_network.py
+++ code/nguyen_network.py
@@ -5,19 +5,19 @@ from network import Network, Commodity, Path
 from right_constant import RightConstant
 
 nguyen_graph = DirectedGraph()
-a = Node(1, nguyen_graph)
-b = Node(2, nguyen_graph)
-c = Node(3, nguyen_graph)
-d = Node(4, nguyen_graph)
-e = Node(5, nguyen_graph)
-f = Node(6, nguyen_graph)
-g = Node(7, nguyen_graph)
-h = Node(8, nguyen_graph)
-i = Node(9, nguyen_graph)
-j = Node(10, nguyen_graph)
-k = Node(11, nguyen_graph)
-l = Node(12, nguyen_graph)
-m = Node(13, nguyen_graph)
+a = Node(0, nguyen_graph)
+b = Node(1, nguyen_graph)
+c = Node(2, nguyen_graph)
+d = Node(3, nguyen_graph)
+e = Node(4, nguyen_graph)
+f = Node(5, nguyen_graph)
+g = Node(6, nguyen_graph)
+h = Node(7, nguyen_graph)
+i = Node(8, nguyen_graph)
+j = Node(9, nguyen_graph)
+k = Node(10, nguyen_graph)
+l = Node(11, nguyen_graph)
+m = Node(12, nguyen_graph)
 
 e_1 = Edge(a,l,1,nguyen_graph)
 e_2 = Edge(a,e,2,nguyen_graph)
@@ -39,7 +39,7 @@ e_17 = Edge(i,m,17,nguyen_graph)
 e_18 = Edge(k,c,18,nguyen_graph)
 e_19 = Edge(m,c,19,nguyen_graph)
 
-nguyen_graph.nodes = {1:a,2:b,3:c,4:d,5:e,6:f,7:g,8:h,9:i,10:j,11:k,12:l,13:m}
+nguyen_graph.nodes = {0:a,1:b,2:c,3:d,4:e,5:f,6:g,7:h,8:i,9:j,10:k,11:l,12:m}
 nguyen_graph.edges = [e_1,e_2,e_3,e_4,e_5,e_6,e_7,e_8,e_9,e_10,e_11,e_12,e_13,e_14,e_15,e_16,e_17,e_18,e_19]
 
 capacities = [5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6,5/6]
